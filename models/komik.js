ecfaf35a5c811105bc30bcb3c7e4f5c03073783c
upi1682 <upi538287@gmail.com>
2026-07-27 14:51:47 +0700

    menambah id

---
+++ models/komik.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, DataTypes) => {
+  const Komik = sequelize.define("Komik", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+   
+    
+  });
+  return Komik;
+};
+
